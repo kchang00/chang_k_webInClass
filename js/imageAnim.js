0d821d5602c9dd9747020d4a7b9f4ff293ed86f4
Chang <k_chang@tss-hr420-dm16.fc.ca>
2019-01-28 15:13:22 -0500

    click headings js

--- js/imageAnim.js
+++ js/imageAnim.js
@@ -1,4 +1,23 @@
 (() => {
+	// select the elements that you want to work with
+
+	// query selector automatically gets first element in section
+	let theButton = document.querySelector("#buttonHolder img");
+	//window.addEventListener("load", changeHeaderText);
+
+	// functions go in the middle
+
+	function changeHeaderText() {
+		document.querySelector("h1").textContent = "I'm taking over.  - Your Friend, JS"
+	}
+
+	function changeSubText() {
+		document.querySelector("#headLine p").textContent = "I'm the best."
+	}
+
+	// event handling goes at the bottom
+	theButton.addEventListener("click", changeHeaderText);
+	theButton.addEventListener("click", changeSubText);
 	// set up the puzzle pieces and boards
-	
+
 })();
